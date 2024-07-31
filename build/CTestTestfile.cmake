45ece0e1732d3867611f1c50deb500c4a5163152
dany <dyagnichenko@gmail.com>
2024-07-31 13:44:55 +0200

    Update project structure. Update CMake.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/volcano/Desktop/15_puzzle_game
-# Build directory: /home/volcano/Desktop/15_puzzle_game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(main_tests "/home/volcano/Desktop/15_puzzle_game/build/main_tests")
-set_tests_properties(main_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/volcano/Desktop/15_puzzle_game/CMakeLists.txt;40;add_test;/home/volcano/Desktop/15_puzzle_game/CMakeLists.txt;0;")
